364ad5b2c958175a5586b49221b42a500621dcc8
Sam-Oliveira <sdo19@ic.ac.uk>
2022-11-08 19:11:52 +0000

    feat: implemented initial portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import { NextPage } from "next"
 import Container from "../components/container"
-
+import Intro from "../components/intro"
+import Projects from "../components/projects"
+import Stack from "../components/intro"
+import GdscCanvas from "../components/gdsc-canvas"
+import AboutMe from "../components/about-me"
 const Home: NextPage = () => {
   return (
     <>
-      <Container>        
+      <Container>
+          <Intro>
 
+          </Intro>
+          <AboutMe>
+            
+          </AboutMe>
+
+          <Projects>
+
+          </Projects>
       </Container>
     </>
   )
